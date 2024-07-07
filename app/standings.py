f2793965fefedca6b3cd94fd494892942214faf7
Ma-Pe-Ma <margitai.peter@gmail.com>
2024-07-07 21:24:37 +0200

    Restructuring standings queries

--- app/standings.py
+++ app/standings.py
@@ -1,79 +1,78 @@
 from flask import Blueprint
 from flask import g
 from flask import render_template
-from flask import current_app
 
 from app.tools.db_handler import get_db
 from app.auth import sign_in_required
 from app.tools import score_calculator 
+from app.tools import time_handler
 from app.tools.cache_handler import cache
 
-from copy import copy
 from sqlalchemy import text
 
 bp = Blueprint('standings', __name__, '''url_prefix="/standings"''')
 
 cache_time = 5
 
-@cache.cached(timeout=cache_time, key_prefix='player_history')
-def create_player_history():
-    #iterate through users
-    query_string = text("SELECT username, REPLACE(:s, '{email_hash}', email_hash) AS image_path FROM bet_user ORDER BY UPPER(bet_user.username)")
-    result = get_db().session.execute(query_string, {'s' : current_app.config['IDENT_URL']})
-
-    players = []
-    for player in result.fetchall():
-        username = player.username
-        days = score_calculator.get_daily_points_by_current_time(username)
-        players.append({'username' : username, 'image_path' : player.image_path, 'days' : days})
-
-    return players
+def create_player_history(select='*', group_by='', order_by=''):
+    user_query = score_calculator.get_daily_points_by_current_time_query(user_filter='', users='SELECT username FROM bet_user ORDER BY UPPER(username)')
+    user_query = '''WITH days AS (
+                        SELECT days.*, 
+                        LAST_VALUE(point) OVER (PARTITION BY days.username) AS last_point,
+	                    LAST_VALUE(penultimate) OVER (PARTITION BY days.username) AS penultimate_point            
+                        FROM (''' + user_query + ''') AS days)
+        
+        SELECT {select}, (penultimate_pos - position) AS position_diff
+        FROM (SELECT days1.date, days1.year, days1.month, days1.day, days1.point, days1.username, days1.point, days1.penultimate, penultimate_point, last_point,
+            REPLACE(:s, '{{email_hash}}', bet_user.email_hash) AS image_path, 
+            (SELECT COUNT(DISTINCT last_point) FROM days AS days2 WHERE days2.last_point >= days1.last_point) AS position,
+            (SELECT COUNT(DISTINCT penultimate_point) FROM days AS days3 WHERE days3.penultimate_point >= days1.penultimate_point) AS penultimate_pos
+            FROM days AS days1
+            LEFT JOIN bet_user ON bet_user.username = days1.username)
+        {group_by}
+        {order_by}
+        '''.format(select=select, group_by=group_by, order_by=order_by)
+    return user_query
 
 @cache.cached(timeout=cache_time, key_prefix='standings')
 def create_standings(language = None):
-    if language is None:
-        language = g.user['language']
+    daily_point_parameters = score_calculator.get_daily_point_parameters()
+    daily_point_parameters.update({'now' : time_handler.get_now_time_object().strftime('%Y-%m-%d %H:%M'), 'l' : language or g.user['language']})
 
-    players = create_player_history()
+    user_query = create_player_history(select='username, image_path, last_point, penultimate_point', group_by='GROUP BY username', order_by='ORDER BY position')
+    all_user_result = get_db().session.execute(text(user_query), daily_point_parameters)
 
-    current_player_standings = [{'username' : player['username'], 'image_path' : player['image_path'], 'point' : player['days'][-1]['point'], 'previous_point' : player['days'][-2]['point'], 'position_diff' : 0} for player in players]
-
-    #order the current player standings by the points
-    current_player_standings.sort(key=lambda player_standing : player_standing['point'], reverse=True)
-
-    #create previous day's standings
-    previous_player_standings = copy(current_player_standings)
-    previous_player_standings.sort(key=lambda prev_player_standing : prev_player_standing['previous_point'], reverse=True)
-
-    for current_position, current_player_standing in enumerate(current_player_standings):
-        position_diff = 0
-
-        for previous_position, previous_player_standing in enumerate(previous_player_standings):
-            if previous_player_standing['username'] == current_player_standing['username']:
-                position_diff = - (current_position - previous_position)
-                break
-
-        current_player_standing['position_diff'] = position_diff
-
-    previous_player_standings = None
-
-    return current_player_standings
+    return [user._asdict() for user in all_user_result.fetchall()]
 
 @bp.route('/standings', methods=['GET'])
 @sign_in_required()
 def standings():
-    standings = create_standings()
-    return render_template('/standings.html', standings=standings)
+    return render_template('/standings.html', standings=create_standings())
 
 @bp.route('/standings.json', methods=['GET'])
 @sign_in_required()
 def standings_json():
+    daily_point_parameters = score_calculator.get_daily_point_parameters()
+    daily_point_parameters.update({'now' : time_handler.get_now_time_object().strftime('%Y-%m-%d %H:%M'), 'l' : g.user['language']})
+
+    user_query = create_player_history(select='username, image_path, date, year, month, day, point', group_by='', order_by='ORDER BY UPPER(username)')
+    all_user_result = get_db().session.execute(text(user_query), daily_point_parameters)
+
+    players = {}
+    for user in all_user_result.fetchall():
+        if user.username not in players:
+            players[user.username] = {'username' : user.username, 'image_path' : user.image_path, 'days' : []}
+
+        day_dict = user._asdict()
+        del day_dict['username']
+        del day_dict['image_path']
+        players[user.username]['days'].append(day_dict)
+
     import io, json
     from flask import send_file
 
-    players = create_player_history()
     in_memory_file = io.BytesIO()
-    in_memory_file.write(json.dumps(players).encode('utf8'))
+    in_memory_file.write(json.dumps(list(players.values())).encode('utf8'))
     in_memory_file.seek(0)
 
     return send_file(in_memory_file, as_attachment=True, download_name='standings.json', mimetype='application/json')
--- app/tools/score_calculator.py
+++ app/tools/score_calculator.py
@@ -3,102 +3,194 @@ from flask import g
 from flask import current_app
 
 from app.tools import time_handler
-from app.tools import group_calculator
 
 from sqlalchemy import text
 
-# this method returns the sum of group bets and the tournament bet of a user
-def get_group_and_tournament_bet_amount(username : str) -> int:    
-    query_string = text("SELECT COALESCE(SUM(group_bet.bet), 0) + COALESCE(tournament_bet.bet, 0) AS total_bet "
-                        "FROM bet_user "
-                        "LEFT JOIN group_bet ON group_bet.username = bet_user.username "
-                        "LEFT JOIN tournament_bet ON tournament_bet.username = bet_user.username "
-                        "WHERE bet_user.username = :username "
-                        "GROUP BY group_bet.username"
-                        )
+daily_point_parameters = {}
 
-    result = get_db().session.execute(query_string, {'username' : username})
+def init_calculator(app):
+    global daily_point_parameters
+    daily_point_parameters = app.config['DEADLINE_TIMES'].copy()
+    daily_point_parameters.update(app.config['GROUP_BET_HIT_MAP'])
+    daily_point_parameters.update(app.config['BONUS_MULTIPLIERS'])
+    daily_point_parameters.update(app.config['BET_VALUES'])
+    daily_point_parameters.update({'starting_bet_amount' : app.config['BET_VALUES']['starting_bet_amount']})
+    daily_point_parameters.update({'s' : app.config['IDENT_URL']})
 
-    return result.fetchone()._asdict()['total_bet']
+def get_daily_point_parameters():
+    return daily_point_parameters.copy()
 
-match_evaluation_query_string = text(
-                            "WITH match_prize AS("
-                                "SELECT match.id, match.outcome AS match_outcome, match_bet.outcome AS bet_outcome, (match.outcome = match_bet.outcome) AS success, "
-                                "match_bet.goal1, match_bet.goal2, match_bet.username AS username, "
-                                    "CASE match.outcome = match_bet.outcome "
-                                        "WHEN 1 THEN CASE match.outcome "
-                                            "WHEN 1 THEN match.odd1 "
-                                            "WHEN 0 THEN match.oddX "
-                                            "WHEN -1 THEN match.odd2 "
-                                            "ELSE 0 END "
-                                        "ELSE 0 "
-                                    "END AS multiplier, "
-                                    "CASE match.outcome = match_bet.outcome "
-                                        "WHEN 1 THEN CASE WHEN match.goal1 = match_bet.goal1 AND match.goal2 = match_bet.goal2 "
-                                            "THEN :bullseye "
-                                            "ELSE CASE WHEN (match.goal1 - match.goal2) = (match_bet.goal1 - match_bet.goal2) AND (match.outcome != 0) "
-                                                "THEN :difference "
-                                                "ELSE 0 "
-                                                "END "
-                                            "END "
-                                        "ELSE 0 "
-                                    "END AS bonus, "
-                                    "COALESCE(match_bet.bet, 0) AS bet "
-                                "FROM (SELECT match.*, SIGN(match.goal1 - match.goal2) AS outcome FROM match) AS match "
-                                "LEFT JOIN (SELECT match_bet.*, SIGN(match_bet.goal1 - match_bet.goal2) AS outcome FROM match_bet) AS match_bet ON match_bet.match_id = match.id "
-                            ")")
+group_and_tournament_bet_query_string = '''SELECT COALESCE(SUM(group_bet.bet), 0) + COALESCE(tournament_bet.bet, 0) AS total_bet, bet_user.username
+                                        FROM bet_user 
+                                        LEFT JOIN group_bet ON group_bet.username = bet_user.username 
+                                        LEFT JOIN tournament_bet ON tournament_bet.username = bet_user.username 
+                                        {filter}
+                                        GROUP BY group_bet.username'''
 
-def get_daily_points_by_current_time(username : str):
-    simple_entry_query = '''SELECT strftime('%Y', date.datetime) AS year, strftime('%m', date.datetime) -1 AS month, strftime('%d', date.datetime) AS day, 
-                    date.datetime, {diff} AS diff 
-                    FROM (SELECT DATE('{st}', '{days}' || ' days') AS datetime) AS date 
+match_evaluation_query_string = '''SELECT match.id, match.outcome AS match_outcome, match_bet.outcome AS bet_outcome, (match.outcome = match_bet.outcome) AS success, 
+                                match_bet.goal1, match_bet.goal2, match_bet.username AS username, 
+                                    CASE match.outcome = match_bet.outcome 
+                                        WHEN 1 THEN CASE match.outcome 
+                                            WHEN 1 THEN match.odd1 
+                                            WHEN 0 THEN match.oddX 
+                                            WHEN -1 THEN match.odd2 
+                                            ELSE 0 END 
+                                        ELSE 0 
+                                    END AS multiplier, 
+                                    CASE match.outcome = match_bet.outcome 
+                                        WHEN 1 THEN CASE WHEN match.goal1 = match_bet.goal1 AND match.goal2 = match_bet.goal2 
+                                            THEN :bullseye 
+                                            ELSE CASE WHEN (match.goal1 - match.goal2) = (match_bet.goal1 - match_bet.goal2) AND (match.outcome != 0) 
+                                                THEN :difference 
+                                                ELSE 0 
+                                                END 
+                                            END 
+                                        ELSE 0 
+                                    END AS bonus, 
+                                    COALESCE(match_bet.bet, 0) AS bet 
+                                FROM (SELECT match.*, SIGN(match.goal1 - match.goal2) AS outcome FROM match) AS match 
+                                LEFT JOIN (SELECT match_bet.*, SIGN(match_bet.goal1 - match_bet.goal2) AS outcome FROM match_bet) AS match_bet ON match_bet.match_id = match.id'''
+
+tournament_dict = '''SELECT t_bet.*, 
+                            CASE t_bet.success WHEN 1 THEN COALESCE(t_bet.bet, 0) * (t_bet.multiplier - 1) ELSE 0 END AS prize, 
+                            COALESCE(t_bet.bet, 0) * (t_bet.multiplier - 1) AS expected_prize 
+                        FROM bet_user 
+                        LEFT JOIN (
+                            SELECT tournament_bet.*, COALESCE(tournament_bet.bet, 0) AS bet, tr.translation AS local_name, 
+                                CASE tournament_bet.result WHEN 0 THEN team.top1 WHEN 1 THEN team.top2 WHEN 2 THEN team.top4 WHEN 3 THEN team.top8 ELSE 0 END AS multiplier 
+                            FROM tournament_bet 
+                            LEFT JOIN team ON team.name = tournament_bet.team 
+                            LEFT JOIN team_translation AS tr ON tr.name = tournament_bet.team AND tr.language = :l 
+                        ) AS t_bet ON t_bet.username = bet_user.username
+                        {filter}
+                    '''
+
+group_dict = '''SELECT ts2.*, COALESCE(ts2.multiplier * ts2.bet, 0) AS prize, (ts2.multiplier - 1) * ts2.bet AS credit_diff 
+            FROM (
+                SELECT ts1.*, CASE ts1.hit_number WHEN 1 THEN :h1 WHEN 2 THEN :h2 WHEN 4 THEN :h4 ELSE 0 END AS multiplier 
+                FROM (SELECT team.*, (team.name = tb.team) AS hit, tr1.translation AS local_name, COALESCE(group_bet.bet, 0) AS bet, 
+                    COALESCE(tb.team, team.name) AS bname, COALESCE(tr2.translation, tr1.translation) AS blocal_name, 
+                    SUM(team.name = tb.team) OVER (PARTITION BY team.group_id, tb.username) AS hit_number, tb.username
+                    FROM team 
+                    LEFT JOIN ( 
+                        SELECT team_bet.position, team.group_id, team_bet.team, bet_user.username
+                        FROM team_bet 
+                        LEFT JOIN team ON team_bet.team = team.name
+                        LEFT JOIN bet_user ON bet_user.username = team_bet.username
+                        {filter}
+                    ) AS tb ON tb.position = team.position AND tb.group_id = team.group_id 
+                    LEFT JOIN group_bet ON group_bet.group_id = team.group_id AND group_bet.username = tb.username
+                    LEFT JOIN team_translation AS tr1 ON tr1.name = team.name AND tr1.language = :l
+                    LEFT JOIN team_translation AS tr2 ON tr2.name = tb.team AND tr2.language = :l
+                    ORDER BY team.group_id, team.position 
+                ) AS ts1 
+            ) AS ts2'''
+
+def get_daily_points_by_current_time_query(user_filter : str, users : str) -> str:
+    simple_entry_query = '''SELECT date_values.*
+                    FROM (SELECT strftime('%Y', date.datetime) AS year, strftime('%m', date.datetime) -1 AS month, strftime('%d', date.datetime) AS day, date.datetime, date(date.datetime) AS date, NULL AS id
+                            FROM (SELECT DATE('{st}', '{days}' || ' days') AS datetime) AS date) AS date_values
                     UNION ALL '''
 
-    match_query = """SELECT * FROM 
-                        (SELECT strftime('%Y', match.datetime) AS year, strftime('%m', match.datetime) -1 AS month, strftime('%d', match.datetime) AS day, MAX(match.datetime) AS datetime,
-                            SUM(COALESCE(match_prize.bonus * match_prize.bet + match_prize.multiplier * match_prize.bet - match_prize.bet, -match_prize.bet, 0)) AS diff
+    match_query = '''SELECT * FROM 
+                        (SELECT strftime('%Y', match.datetime) AS year, strftime('%m', match.datetime) -1 AS month, strftime('%d', match.datetime) AS day, match.datetime AS datetime, date(match.datetime) AS date, match.id AS id
                         FROM match
-                        RIGHT JOIN bet_user
-                        LEFT JOIN match_prize ON match_prize.id = match.id AND match_prize.username = bet_user.username
-                        WHERE unixepoch(datetime) < unixepoch(:now) AND unixepoch(datetime) {r} unixepoch(:group_evaluation_time) AND bet_user.username = :u
-                        GROUP BY date(match.datetime)
+                        WHERE unixepoch(datetime) < unixepoch(:now) AND unixepoch(datetime) {r} unixepoch(:group_evaluation)
                         ORDER BY datetime)
-                    UNION ALL """
+                    UNION ALL '''
 
     deadline_times = current_app.config['DEADLINE_TIMES']
 
-    # add match_evaluation CTE
-    complete_query = match_evaluation_query_string.text
     # add starting credit
-    complete_query += simple_entry_query.format(st=deadline_times['register'], days=-2, diff=0)
+    complete_query = simple_entry_query.format(st=deadline_times['register'], days=-2)
     # subtract group + tournament bet credits
-    complete_query += simple_entry_query.format(st=deadline_times['register'], days=-1, diff=-get_group_and_tournament_bet_amount(username))
+    complete_query += simple_entry_query.format(st=deadline_times['register'], days=-1)
     # calculate group stage results
     complete_query += match_query.format(r='<')
-    # add group stage bonus
-    complete_query += simple_entry_query.format(st=deadline_times['group_evaluation'], days=1, diff= sum(group['prize'] for group in group_calculator.get_group_bet_dict_for_user(username=username).values()))
-    # calculate knock-out stage results
+    # # add group stage bonus
+    complete_query += simple_entry_query.format(st=deadline_times['group_evaluation'], days=1)
+    # # calculate knock-out stage results
     complete_query += match_query.format(r='>')
-    # add tournament bonus
-    complete_query += simple_entry_query.format(st=deadline_times['tournament_end'], days=1, diff=group_calculator.get_tournament_bet_dict_for_user(username=username, language=g.user['language'])['prize'])
+    # # add tournament bonus
+    complete_query += simple_entry_query.format(st=deadline_times['tournament_end'], days=1)
     # add empty SELECT after UNION ALL
-    complete_query += 'SELECT 1, 2, 3, 4, 5 WHERE 0 = 1'
-    
-    hitmap = current_app.config['BONUS_MULTIPLIERS']
-    daily_point_parameters = {
-        'now' : time_handler.get_now_time_object().strftime('%Y-%m-%d %H:%M'),
-        'group_evaluation_time' : deadline_times['group_evaluation'],
-        'starting_point' : current_app.config['BET_VALUES']['starting_bet_amount'],
-        'u' : username,
-        'bullseye' : hitmap['bullseye'], 'difference' : hitmap['difference']
-    }
+    complete_query += 'SELECT 1, 2, 3, 4, 5, 6 WHERE 0 = 1'
 
-    complete_query =  '''
-    SELECT date(days.datetime) AS date, days.year, days.month, days.day,
-        COALESCE(:starting_point + SUM(diff) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND 0 PRECEDING), :starting_point) AS point
+    complete_query = '''WITH
+        match_prize AS (''' + match_evaluation_query_string + '''),
+        gt_bet_amount AS (''' + group_and_tournament_bet_query_string.format(filter='') +'''),
+        group_dict AS (''' + group_dict.format(filter='') + '''),
+        tournament_dict AS (''' + tournament_dict.format(filter='') + ''')
+
+    SELECT point_entries.*, bet_user.username, 
+        CASE point_entries.date
+            WHEN date(:register, '-2 days') THEN :starting_bet_amount
+            WHEN date(:register, '-1 days') THEN -gt_bet_amount.total_bet
+            WHEN date(:group_evaluation, '1 days') THEN group_bonus.prize
+            WHEN date(:tournament_end, '1 days') THEN tournament_bonus.prize
+            ELSE SUM(COALESCE(match_prize.bonus * match_prize.bet + match_prize.multiplier * match_prize.bet - match_prize.bet, -match_prize.bet, 0))
+        END AS diff
+    FROM (''' + complete_query + ''') AS point_entries
+    LEFT JOIN bet_user ON bet_user.username IN({users})
+    LEFT JOIN match_prize ON match_prize.username = bet_user.username AND point_entries.id = match_prize.id
+    LEFT JOIN gt_bet_amount ON bet_user.username = gt_bet_amount.username
+    LEFT JOIN (SELECT username, SUM(prize) AS prize FROM (SELECT * FROM group_dict GROUP BY username, group_id) GROUP BY username) AS group_bonus ON group_bonus.username = bet_user.username
+    LEFT JOIN tournament_dict AS tournament_bonus ON tournament_bonus.username = bet_user.username
+    GROUP BY bet_user.username, point_entries.date'''.format(users=users)
+    
+    # calculate the player's points at the end of the days
+    complete_query = '''
+    SELECT days.username, date(days.datetime) AS date, days.year, days.month, days.day,
+        diff + COALESCE(SUM(diff) OVER (PARTITION BY username ORDER BY username ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING), 0) AS point,
+        diff AS diff,
+        COALESCE(SUM(diff) OVER (PARTITION BY username ORDER BY username ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING), 0) as penultimate
     FROM (''' + complete_query + ''') AS days
     WHERE unixepoch(datetime) < unixepoch(:now)'''
 
+    return complete_query
+
+def get_daily_points_by_current_time(username):    
+    daily_point_parameters = get_daily_point_parameters()
+    daily_point_parameters.update({'u' : username, 'l' : g.user['language'], 'now' : time_handler.get_now_time_object().strftime('%Y-%m-%d %H:%M')})
+
+    complete_query = get_daily_points_by_current_time_query(user_filter='AND bet_user.username = :u', users=':u')
     day_result = get_db().session.execute(text(complete_query), daily_point_parameters)
 
     return [day._asdict() for day in day_result.fetchall()]
+
+# this method returns the sum of group bets and the tournament bet of a user
+def get_group_and_tournament_bet_amount(username : str) -> int:
+    query_string  = group_and_tournament_bet_query_string.format(filter='WHERE group_bet.username = :username')
+    query_string = " SELECT * FROM ({q})".format(q=query_string)
+    result = get_db().session.execute(text(query_string), {'username' : username})
+
+    return result.fetchone()._asdict()['total_bet']
+
+# get's user's tournament bet, or create default if it does not exist
+def get_tournament_bet_dict_for_user(username : str, language = None) -> dict:
+    query_string = text(tournament_dict.format(filter="WHERE bet_user.username = :u"))
+    return get_db().session.execute(query_string, {'u' : username, 'l' : language or g.user['language']}).fetchone()._asdict()
+
+# get group object which contains both the results and both the user bets (used in every 3 contexts)
+def get_group_bet_dict_for_user(username : str, language = None):
+    query_string = text(group_dict.format(filter="WHERE team_bet.username = :u"))
+
+    hit_map = current_app.config['GROUP_BET_HIT_MAP']
+
+    result = get_db().session.execute(query_string, {'u' : username, 'l' : language or g.user['language'], 'h1' : hit_map['h1'], 'h2' : hit_map['h2'], 'h4' : hit_map['h4']})
+    team_rows = result.fetchall()
+
+    groups = {}
+    for team_row in team_rows:
+        if team_row.group_id not in groups:
+            groups[team_row.group_id] = {'hit_number' : team_row.hit_number, 'bet' : team_row.bet, 'prize' : team_row.prize, 'credit_diff' : team_row.credit_diff, 'teams' : [], 'multiplier' : team_row.multiplier}
+        
+        team_dict = team_row._asdict()
+        del team_dict['hit_number']
+        del team_dict['bet']
+        del team_dict['prize']
+        del team_dict['multiplier']
+
+        groups[team_row.group_id]['teams'].append(team_dict)
+
+    return groups
